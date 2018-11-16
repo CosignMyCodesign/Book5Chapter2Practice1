b40a7fd332b7891555ec43ead67273bd5a833d4b
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-11-16 11:41:47 -0600

    made my first simple React App

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+export default class LocationList  extends Component {
+    render() {
+        return (
+            <article>
+                <h1>Location List</h1>
+                <section>Nashville North</section>
+                  <p>123 North North Street</p>
+                <section>Nashville South</section>
+                  <p>321 South South Street</p>
+            </article>
+        );
+    }
+}
